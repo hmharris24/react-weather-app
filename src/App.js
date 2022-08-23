16e10bf364c2ae782c584e4b909db05473cb7f2c
hmharris24 <102563834+hmharris24@users.noreply.github.com>
2022-08-22 21:09:07 -0500

    Added GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -2,5 +2,22 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by{" "}
+        <a href="https://www.linkedin.com/in/holly-meyers/" target="_blank">
+          Holly Meyers
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/hmharris24/react-weather-app"
+          target="_blank"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
