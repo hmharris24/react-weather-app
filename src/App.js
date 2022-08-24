be76c3dc67c402a59f71dd9930a69c6adca5c94d
hmharris24 <102563834+hmharris24@users.noreply.github.com>
2022-08-23 20:42:34 -0500

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -4,20 +4,27 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by{" "}
-        <a href="https://www.linkedin.com/in/holly-meyers/" target="_blank">
-          Holly Meyers
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/hmharris24/react-weather-app"
-          target="_blank"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://www.linkedin.com/in/holly-meyers/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Holly Meyers
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/hmharris24/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
